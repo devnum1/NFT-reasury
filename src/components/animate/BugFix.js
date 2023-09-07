1a68c61d11f8a14aabbe2898ae57fea31821ff1a
Deterrion Quintez Corley <137900476+deterrion-corley-tech@users.noreply.github.com>
2023-09-07 06:12:54 -0400

    BugFix commponent

---
+++ src/components/animate/BugFix.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function BugFix() {
+    return (
+        <>
+            <div>
+                <h2>Hello, Nice to meet you!</h2>
+                <p>I am a senior full stack developer with nealy ten years of experience in software development.</p>
+            </div>
+        </>
+    )
+}
